21354a327d45a96aade7d15601b0cf84d160a51e
McDonaldsOfficial <69216135+McDonaldsOfficial@users.noreply.github.com>
2022-04-04 23:15:33 -0400

    Cambios en el carrusel

--- src/Components/Carousel/CC.js
+++ src/Components/Carousel/CC.js
@@ -26,9 +26,15 @@ const CC = () =>{
                 <Carousel breakPoints={breakPoints} >
                     <Item>
                         <img width='140px' height='140px' style={{borderRadius:'50%'}}/>
-                        <span>Me encantan los abrazos</span>
-                        <b>Gustavo Valda</b>
-                        <i>Developer</i>
+                        <span></span>
+                        <b>ING. Marco Avendaño Ajata</b>
+                        <i>Product Owner</i>
+                    </Item>
+                    <Item>
+                        <img width='140px' height='140px' style={{borderRadius:'50%'}}/>
+                        <span>Los orangutanes son mis primos</span>
+                        <b>Dylan Salazar</b>
+                        <i>eduScrum Master</i>
                     </Item>
                     <Item>
                         <img width='140px' height='140px' style={{borderRadius:'50%'}}/>
@@ -38,26 +44,20 @@ const CC = () =>{
                     </Item>
                     <Item>
                         <img width='140px' height='140px' style={{borderRadius:'50%'}}/>
-                        <span>Me encantan los abrazos</span>
-                        <b>Gustavo Valda</b>
-                        <i>Developer</i>
+                        <span>Plata en el lol, pero maestro en el estudio</span>
+                        <b>Orual Aranda Claure</b>
+                        <i>Diseñador &amp; Developer</i>
                     </Item>
                     <Item>
                         <img width='140px' height='140px' style={{borderRadius:'50%'}}/>
-                        <span>Me encantan los abrazos</span>
-                        <b>Gustavo Valda</b>
-                        <i>Developer</i>
+                        <span>Futuro Ing.Sistemas, millonario, genio, filántropo y Playboy</span>
+                        <b>Luis Esteban Arteaga</b>
+                        <i>Diseñador</i>
                     </Item>
                     <Item>
                         <img width='140px' height='140px' style={{borderRadius:'50%'}}/>
-                        <span>Me encantan los abrazos</span>
-                        <b>Gustavo Valda</b>
-                        <i>Developer</i>
-                    </Item>
-                    <Item>
-                        <img width='140px' height='140px' style={{borderRadius:'50%'}}/>
-                        <span>Me encantan los abrazos</span>
-                        <b>Gustavo Valda</b>
+                        <span>El verdadero legionario que toma Coca Cola con pan</span>
+                        <b>Alexander Joel Calle</b>
                         <i>Developer</i>
                     </Item>                 
                 </Carousel>
