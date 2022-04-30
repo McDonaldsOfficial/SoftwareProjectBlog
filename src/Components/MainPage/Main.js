8802db0a6b00c8a7ddc539e49339fad6854af2e8
McDonaldsOfficial <69216135+McDonaldsOfficial@users.noreply.github.com>
2022-04-30 00:46:14 -0400

    Sprint 5

--- src/Components/MainPage/Main.js
+++ src/Components/MainPage/Main.js
@@ -10,7 +10,8 @@ const images  = [
 'https://www.monday.com/blog/wp-content/uploads/2021/06/cg45imeetz97Bi1aO7F7Hczm_okRzkD90cI_n85UntHmyphNEJH2J1DmAO1KPt0jjIPLAmCsKannpMIDta9armoNXipQOrsCRxDQPwBR7rjcawY6BD4-HxQDqxsgSQAFoA2LQfvT.png',
 'https://d112uwirao0vo9.cloudfront.net/wp-content/uploads/2021/08/scrum-methodology.png',
 'http://www.cidi.com.ar/assets/images/servicioPM.jpg',
-'https://www.ealde.es/wp-content/uploads/2017/12/direccion-de-proyectos-gestion-del-cambio.jpg']
+'https://www.ealde.es/wp-content/uploads/2017/12/direccion-de-proyectos-gestion-del-cambio.jpg',
+'https://www.isotools.org/wp-content/uploads/2020/06/Software-GRC.jpg']
 
 function Main() {
   return (
@@ -42,6 +43,7 @@ function Main() {
           <Sprint number={'2'} content={'Metodologías de administración de proyectos de Software'} image={images[1]} url={'w'} />
           <Sprint number={'3'} content={'Metodologías y PMBOK'} image={images[2]} url={'w'} />          
           <Sprint number={'4'} content={'Administrando cambios del proyecto'} image={images[3]} url={'https://62518d7180793.site123.me/'} />
+          <Sprint number={'5'} content={'Gestionando riesgos'} image={images[4]} url={'https://62686f70d7f85.site123.me/'} />
         </div>     
       </div>
       <Footer/>
